f21294f0f4f4c736c6d16783f839bb69905d6ac6
Smartex <nurgul.aisariyeva@smartex.kz>
2021-08-12 14:26:33 +0600

    Initial Commit

---
+++ Sample Demo App iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample Demo App iOS
+//
+//  Created by smartex on 12.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

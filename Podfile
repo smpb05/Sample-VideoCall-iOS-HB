9b7edbc94fd8a7525f094c913f8470f0456b8f9f
Smartex <nurgul.aisariyeva@smartex.kz>
2021-08-13 17:05:51 +0600

    Sample for iOS app

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Sample Demo App iOS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Sample Demo App iOS
+  pod 'TSAVideoCallSDKHB', :git => 'https://github.com/smpb05/TSAVideoCallSDKHB.git', :branch => 'main', :tag => '0.2.6'
+
+
+end
--- Sample Demo App iOS/ViewController.swift
+++ Sample Demo App iOS/ViewController.swift
@@ -6,14 +6,116 @@
 //
 
 import UIKit
+import TSAVideoCallSDKHB
 
 class ViewController: UIViewController {
 
+    var session: TSAVideoCallSession?
+    var publisher: TSAVideoCallPublisher?
+    var subscriber: TSAVideoCallSubscriber?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let config = TSAVideoCallConfig(webUrl: "enter-url",
+                                        webSocketMediaServerUrl: "enter-url",
+                                        webSocketBrokerUrl: "enter-url",
+                                        webSocketBrokerPath: "enter-path",
+                                        callHash: "enter-callHash",
+                                        authData: "enter-credentials")
+        
+       initSession(config: config)
     }
 
 
+    private func initSession(config: TSAVideoCallConfig){
+        session = TSAVideoCallSession(config: config)
+        session?.sessionDelegate = self
+        session?.connect()
+    }
+    
+    
+    private func initializePublisher( _ session: TSAVideoCallSession){
+        publisher = TSAVideoCallPublisher(session: session)
+        if let publisher = publisher{
+            publisher.delegate = self
+            session.publish(publisher: publisher)
+        }
+    }
+    
+    private func initializeSubscriber(session: TSAVideoCallSession, stream: TSAVideoCallStream){
+        subscriber = TSAVideoCallSubscriber(session: session, stream: stream, frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        subscriber?.delegate = self
+        if let subscriber = subscriber{
+            session.subscribe(subscriber: subscriber)
+            view.addSubview(subscriber.getVideoView())
+        }
+    }
+    
 }
+
+extension ViewController: TSAVideoCallSessionDelegate, TSAVideoCallPublisherDelegate, TSAVideoCallSubscriberDelegate{
+    
+    //publisher
+    func onStreamCreated(publisher: TSAVideoCallPublisher) {
+        let publisherVideoView = publisher.getVideoView()
+        publisherVideoView.frame = CGRect(x: view.frame.width*0.7, y: view.frame.height*0.7, width: view.frame.width*0.4, height: view.frame.height*0.3)
+        publisherVideoView.layer.zPosition = 1
+        view.addSubview(publisherVideoView)
+    }
+    
+    func onStreamDestroyed(publisher: TSAVideoCallPublisher) {
+        
+    }
+    
+    func onError(publisher: TSAVideoCallPublisher, error: TSAVideoCallError) {
+        
+    }
+    
+    
+    //subscriber
+    func onConnected(subscriber: TSAVideoCallSubscriber) {
+        
+    }
+    
+    func onDisconnected(subcriber: TSAVideoCallSubscriber) {
+        
+    }
+    
+    func onError(subscriber: TSAVideoCallSubscriber, error: TSAVideoCallError) {
+        
+    }
+    
+    
+    //session
+    func onConnected(session: TSAVideoCallSession) {
+        initializePublisher(session)
+    }
+    
+    func onDisconnected(session: TSAVideoCallSession) {
+        
+    }
+    
+    func onStreamReceived(session: TSAVideoCallSession, stream: TSAVideoCallStream) {
+        initializeSubscriber(session: session, stream: stream)
+    }
+    
+    func onStreamDropped(session: TSAVideoCallSession, stream: TSAVideoCallStream) {
+        
+    }
+    
+    func onMessageReceived(session: TSAVideoCallSession, message: String) {
+        
+    }
+    
+    func onFileReceived(session: TSAVideoCallSession, fileName: String, filePath: String) {
+        
+    }
+    
+    func onError(session: TSAVideoCallSession, error: TSAVideoCallError) {
+        
+    }
+    
+}
+
 
